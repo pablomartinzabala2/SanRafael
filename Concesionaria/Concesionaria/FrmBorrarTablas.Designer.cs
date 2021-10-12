275bf29fb9a8a2fb822732a85aefaf7bc47b4d65
pablozabala <pablozabala.jakemate@gmail.com>
2021-10-12 18:20:42 -0300

    ASD
    ASD

--- Concesionaria/Concesionaria/FrmBorrarTablas.Designer.cs
+++ Concesionaria/Concesionaria/FrmBorrarTablas.Designer.cs
@@ -34,7 +34,6 @@
             // 
             // button1
             // 
-            this.button1.Enabled = false;
             this.button1.Location = new System.Drawing.Point(93, 49);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(103, 43);
